8923886eda57fcdabc4b465d4b4674c4779c61ea
AndySouth <southandy@gmail.com>
2020-03-19 22:20:51 +0000

    add UI urls

--- inst/shiny/healthsites_viewer/ui.r
+++ inst/shiny/healthsites_viewer/ui.r
@@ -21,8 +21,20 @@ library(afrihealthsites)
 
 pageWithSidebar(
   headerPanel('healthsites in Africa, from healthsites.io and WHO'),
-  sidebarPanel( width=2,
-    selectInput('country', 'Country', afcountries$name)
+  sidebarPanel( width=3,
+    selectInput('country', 'Country', afcountries$name, size=10, selectize=FALSE, multiple=TRUE, selected="Angola"),
+
+    #p("This app was developed by", a(tags$strong("data-analysis OG"), href="http://www.data-analysis.at", target="_blank")),
+
+    p("Developed by ", a("afrimapr", href="http://www.afrimapr.org", target="_blank")),
+    p("Open source ", a("R code", href="https://github.com/afrimapr/afrihealthsites", target="_blank")),
+
+    p("under development March 2020, some countries not yet working"),
+
+    p("Compares data from :\n"),
+    p(a("healthsites.io", href="https://www.healthsites.io", target="_blank")),
+    p("\n", a("WHO", href="https://www.who.int/malaria/areas/surveillance/public-sector-health-facilities-ss-africa/en/", target="_blank")),
+
   ),
   mainPanel(
     leafletOutput("serve_healthsites_map", height=1000)
